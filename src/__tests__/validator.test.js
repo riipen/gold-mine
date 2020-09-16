9fdadddc753262318d35d6b7e8efab5b2e2bc78c
daelynj <daelyndbjones@gmail.com>
2020-09-16 14:58:49 -0700

    Add base test for validator.js

---
+++ src/__tests__/validator.test.js
@@ -0,0 +1,18 @@
+import validator from "../validator.js";
+import runner from "../runner.js";
+import mine from "../../mines/jupiter.js";
+import path from "path";
+import fs from "fs";
+
+describe("validate", () => {
+  test("it runs the mine and validates that the steps and score are valid", async () => {
+    const logFile = path.join(__dirname, "logs", `validator_test.txt`);
+
+    const mineScore = await runner.run(mine, logFile);
+    const valid = await validator.validate(mine, logFile, mineScore);
+
+    expect(valid).toBe(true);
+
+    fs.writeFileSync(logFile, "");
+  });
+});
