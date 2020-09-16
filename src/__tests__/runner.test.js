722233e1e67d1cba1bf82db901054fc6f3121cb1
daelynj <daelyndbjones@gmail.com>
2020-09-16 14:58:18 -0700

    Add base test for runner.js

---
+++ src/__tests__/runner.test.js
@@ -0,0 +1,15 @@
+import runner from "../runner.js";
+import mine from "../../mines/jupiter.js";
+import path from "path";
+import fs from "fs";
+
+describe("run", () => {
+  test("it runs the mine and returns the score", async () => {
+    const logFile = path.join(__dirname, "logs", `runner_test.txt`);
+    let score = await runner.run(mine, logFile);
+
+    expect(score).toEqual(9);
+
+    fs.writeFileSync(logFile, "");
+  });
+});
