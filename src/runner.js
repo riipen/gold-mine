ddf294426cb41c0c8e76ca8b67cf174d2f276246
theanh28 <56032607+theanh28@users.noreply.github.com>
2020-12-04 02:56:54 +0700

    Upload for testing first. Will provide full ASAP

--- src/runner.js
+++ src/runner.js
@@ -1,6 +1,7 @@
 import fs from "fs";
-
+import Position from "./position.js";
 import move from "./move.js";
+import { ppid } from "process";
 
 /**
  * Given a mine, runs the miner through the mine collecting gold along the way.
@@ -11,41 +12,102 @@ import move from "./move.js";
  *
  * @return {Number} The total gold collected by the miner.
  */
+
+function optimize(a, modeA, b, modeB, val) {
+  if (a > b) return [a + val, modeA];
+  return [b + val, modeB];
+}
+function update(ans, val, x, y) {
+  if (ans.val > val) return;
+  ans.val = val;
+  ans.x = x;
+  ans.y = y;
+}
+
 const run = async (mine, logFile, yStart = 0) => {
   if (!mine) throw new Error("a mine is required");
   if (!logFile) throw new Error("a logFile is required");
-
-  // Initial position
-  let position = await move(mine);
-
-  // Track where the current X value should be
-  let currentX = 0;
-
-  // A running tally of the score
-  let score = mine[position.y][position.x];
-
-  // log the initial position
-  log(logFile, position);
-
-  while (position.x < mine[0].length - 1 && position.isValid(mine)) {
-    if (position.x !== currentX) {
-      throw new Error(
-        `Current position must be at x === ${currentX}, not ${position}`
-      );
-    }
-
-    position = await move(mine, position);
-    currentX++;
-
-    log(logFile, position);
-
-    if (!position.isValid(mine) || mine[position.y][position.x] === 0) break;
-
-    score += mine[position.y][position.x];
+  //---------------
+  /* print out a[] for debug
+  let p = [];
+  for (let i = 0; i < mine[0].length; ++i) {
+      p.push([]);
+      for (let j = 0; j < mine.length; ++j)
+          p[i].push(0);
   }
+  */
+  //---------------
+  let a = [ [ [], [], [] ] ]; //our BFS value board
+  let t = [ [ [], [], [] ] ]; //previous MOVE to backtrack from final position
+  for (let i = 0; i < mine.length; ++i) { //initial move
+      a[0][0].push(mine[i][0]); //filling array
+      a[0][1].push(mine[i][0]);
+      a[0][2].push(mine[i][0]);
+      t[0][0].push(-1); //filling backtrack
+      t[0][1].push(-1);
+      t[0][2].push(-1);
+  }
+  //-----------
+  let ans = {val: 0, m: -1 ,  x: -1, y: -1}; // result: score + final position
 
-  return score;
-};
+  for (let col = 0; col < mine[0].length; ++col) {
+      a.push([ [], [], [] ]); // filling array
+      t.push([ [], [], [] ]);
+      for (let i = 0; i < mine.length; ++i) {
+          a[col + 1][0].push(-1e9); t[col + 1][0].push(-1); //filling array
+          a[col + 1][1].push(-1e9); t[col + 1][1].push(-1);
+          a[col + 1][2].push(-1e9); t[col + 1][2].push(-1);
+      }
+      for (let i = 0; i < mine.length; ++i) {
+          //p[i][col] = Math.max(a[col][0][i], a[col][1][i], a[col][2][i]); 
+          //print out a[] for debug
+          if (mine[i][col] == 0 || col + 1 == mine[0].length) {
+              update(ans, a[col][0][i], col, i);
+              update(ans, a[col][1][i], col, i);
+              update(ans, a[col][2][i], col, i);
+              continue;
+          }
+          let temp;
+          if (i > 0) {
+              temp = optimize(a[col][1][i], 1, a[col][2][i], 2, mine[i - 1][col + 1]);
+              a[col + 1][0][i - 1] = temp[0];     t[col + 1][0][i - 1] = temp[1];
+          } else {
+              update(ans, a[col][0][i], col, i);
+              update(ans, a[col][1][i], col, i);
+          }
+          //---
+          temp = optimize(a[col][0][i], 0, a[col][2][i], 2, mine[i][col + 1]);
+          a[col + 1][1][i] = temp[0];     t[col + 1][1][i] = temp[1];
+          //---
+          if (i < mine.length - 1) {
+              temp = optimize(a[col][0][i], 0, a[col][1][i], 1, mine[i + 1][col + 1]);
+              a[col + 1][2][i + 1] = temp[0];     t[col + 1][2][i + 1] = temp[1];
+          } else {
+              update(ans, a[col][1][i], col, i);
+              update(ans, a[col][2][i], col, i);
+          }
+      }
+  }
+  //-------------------
+  let trace = [];
+  if (!(ans.y >=0 && ans.y < mine.length && !mine[ans.y][ans.x])){
+      if (ans.y == 0) 
+          trace.push(`${ans.x + 1},${ans.y - 1}`);
+      else
+          trace.push(`${ans.x + 1},${ans.y + 1}`);
+  }
+  while (ans.x >= 0) {
+      //console.log(ans.x, ans.y, ans.m);
+      trace.push(`${ans.x},${ans.y}`);
+      let temp = ans.y;
+      ans.y -= (ans.m - 1); // 0 -> + 1, 1 -> 0, 2 -> -1
+      ans.m = t[ans.x][ans.m][temp];
+      -- ans.x;
+  }
+  for (let i = trace.length - 1; i >= 0; --i)
+      log(logFile, trace[i]);
+  console.log(ans.val);
+}
 
 /**
  * Logs the miner's current position to a log file. This file is used for validation
@@ -57,7 +119,7 @@ const run = async (mine, logFile, yStart = 0) => {
  * @return {undefined}
  */
 const log = (logFile, position) => {
-  fs.appendFileSync(logFile, `${position.toString()}\n`);
+  fs.appendFileSync(logFile, `${position}\n`);
 };
 
 export default {
