c0bad7e5ba77bfc533af77cf8d64f072f41366a3
Eric Rojo <ericrojo156@hotmail.com>
2021-02-14 21:24:24 -0800

    add more comments/documentation to move.js

--- src/move.js
+++ src/move.js
@@ -1,8 +1,9 @@
 import MinerPath from "./MinerPath.js";
 import Position from "./position.js";
 
-// maximum mining depth multiple in x axis precognition of an optimum path through a given segment of the mine
-let precogDepthX = 10;
+// maximum depth in the x-component of a segment of the overall mine,
+// to impose limits on recursion depth when performing backtracking to find optimal path
+let mineSegmentDepthX = 10;
 
 // number of explored mine segments for the run of the algorithm
 let exploredMineSegments = 1;
@@ -17,47 +18,59 @@ export const Moves = Object.freeze({
 // list to contain the positions of the miner along the optimum path (global for all calls to move())
 let optimumPathPositionsStack = [];
 
-
 /**
- * 
+ * This function uses a recursive backtracking algorithm to determine the optimal
+ * path through a mine segment with x-component depth precogDepthX,
+ * to retrieve as much gold as possible from the mine segment.
  * @param {array} mine  - A n x m multidimensional array respresenting the mine.
  * @param {object} position - The current position of the miner
  * @param {object} previousMove - The previous move of the miner to reach the current position
  */
 export const findOptimumPath = async (mine, position, previousMove = null) => {
-  if (mine.length > 100) {
-    precogDepthX = 5;
-  }
   let maxKnownGold = 0;
   let chosenMinerPath = new MinerPath();
+  // select available moves by filtering out the previous move,
+  // or simply accepting them all if no previous move was performed (ie. if this is the first position)
   const availableMoves = Object.values(Moves).filter(aMove => previousMove === null || aMove.y !== previousMove.y);
-  // explore path options available to the given position based on available moves
+  // explore path options available to the given position based on available moves.
   const pathOptionsPromises = availableMoves.map(
     async (aMove) => {
+      // use the move object as a position delta to calculate the next position
       const nextPosition = new Position(position.x + aMove.x, position.y + aMove.y);
+      // count the gold at the next position if the position is valid
       const foundGold = (nextPosition.isValid(mine) && mine[nextPosition.y][nextPosition.x]) || 0;
-      if (((position.x % (exploredMineSegments * precogDepthX)) === 0 && position.x !== 0) || nextPosition.x >= mine[0].length || nextPosition.y >= mine.length) {
+      // determine if the current path has reached the end of the mine segment being currently explored
+      const isAtLimitsOfMineSegment = (position.x % (exploredMineSegments * mineSegmentDepthX)) === 0;
+      // determine if the current position is in the first column of the entire mine
+      const isNotFirstPositionInMine = position.x !== 0;
+      // determine if the next position being considered is beyond the bounds of the mine
+      const nextPositionIsBeyondMineBounds = (nextPosition.x >= mine[0].length) || (nextPosition.y >= mine.length);
+      if ((isAtLimitsOfMineSegment && isNotFirstPositionInMine) || nextPositionIsBeyondMineBounds) {
         return new MinerPath([], foundGold);
       }
-      if (foundGold === 0) { // base case: next position is the end of a valid path through the mine
+      // base case: 0 gold for an invalid/end position, therefore consider it the end of the prospective path
+      if (foundGold === 0) {
         return new MinerPath(
           [nextPosition],
           foundGold
         );
-      } else { // recursive case: explore branching of new moves at the new position following the current move aMove
+      } else {
+        // recursive case: explore branching of new moves at the new position following the current move
+        // (passing the current move as the previous move for the next search tree)
         return await findOptimumPath(mine, nextPosition, aMove);
       }
     }
   );
-  // wait for all paths to be computed in parallel
+  // wait for all asynchronously computed paths to be completed
   const possibleMinerPaths = await Promise.all(pathOptionsPromises);
-  // choose the path that yields the maximum known gold
+  // choose the path that yields the most gold
   possibleMinerPaths.forEach(path => {
     if (path.goldFromPath >= maxKnownGold) {
       maxKnownGold = path.goldFromPath;
       chosenMinerPath = path;
     }
   });
+  // push the current position to the path positions list at the given level of the recursive search path
   chosenMinerPath.pathPositions.push(position);
   return new MinerPath(
     chosenMinerPath.pathPositions, // assign the chosen path positions sequence to the returned path
@@ -74,31 +87,48 @@ export const findOptimumPath = async (mine, position, previousMove = null) => {
  *
  * @param  {array} mine - A n x m multidimensional array respresenting the mine.
  * @param  {object} position - The current position of the miner, will be undefined on the first move
- *
+ * @param  {number} yStart - The y-component of the miner's start position in column x = 0
  * @return {Position} The new position of the miner.
  */
-const move = async (mine, position) => {
-  const isAtFirstPosition = typeof(position) === 'undefined';
-  const pathComputationIsRequired = optimumPathPositionsStack.length === 1;
-  const atEdgeOfMineSegment = !isAtFirstPosition && pathComputationIsRequired;
+const move = async (mine, position = null, yStart = null) => {
+  // for mines at least 100 rows in length, decrease the mine segment depth as a trade-off in favour of better performance
+  if (mine.length >= 100) {
+    mineSegmentDepthX = 5;
+  }
+  const isAtFirstPosition = position === null;
+  // if the miner is at the first position of the mine, decide what position he/she should take to begin their exploration
   if (isAtFirstPosition) {
-    const randomY = Math.random() * (mine.length);
-    position = new Position(0, Math.floor(randomY));
+    // if the y-component of the start position is specified, use it in the start position
+    if (yStart !== null) {
+      position = new Position(0, yStart)
+    } else {
+      // if no y-component is spectified for the starting position, assign a random position along y in the first column of the mine
+      const randomY = Math.random() * (mine.length);
+      position = new Position(0, Math.floor(randomY));
+    }
+    // the number of explored mine segments starts at 1 when the miner is in the first position of the overall mine
     exploredMineSegments = 1;
   }
-  if (atEdgeOfMineSegment) { // case where the miner has completed traversal of a mine segment
+  // determine if there is need to compute the optimal path for the mine segment ahead,
+  // in the case that the miner is reaching the end of the currently known path.
+  const pathComputationIsRequired = optimumPathPositionsStack.length <= 1;
+  // increment the number of mine segments that have been explored if the miner is ready to explore the next mine segment
+  if (!isAtFirstPosition && pathComputationIsRequired) {
     exploredMineSegments += 1;
   }
   if (pathComputationIsRequired || isAtFirstPosition) {
     let prevMove = null;
     if (!isAtFirstPosition) {
       const nextPosition = optimumPathPositionsStack.pop();
+      // dynamically compute the previous move to pass to findOptimumPath, based on the next position and the current position of the path
       prevMove = new Position(nextPosition.x - position.x, nextPosition.y - position.y);
       position = nextPosition;
     }
     optimumPathPositionsStack = (await findOptimumPath(mine, position, prevMove)).pathPositions;
-    optimumPathPositionsStack.pop(); // exclude the initial position in the path
+    // the initial position in the path positions stack is disregarded at the root level call of findOptimumPath, since the miner is already at this position
+    optimumPathPositionsStack.pop();
   } else {
+    // assign the position to the next position dictated by the optimum path positions stack
     position = optimumPathPositionsStack.pop();
   }
   return position;
