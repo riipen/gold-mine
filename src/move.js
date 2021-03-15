cc97857a1587c41382fadfe832486e3d87907a53
Tais Duarte <tais.duarte@creditas.com.br>
2021-03-14 22:54:57 -0300

    [ feature ] Initial move logic.

--- src/move.js
+++ src/move.js
@@ -1,6 +1,10 @@
 import Position from "./position.js";
 
-let movedRight;
+let firstMove = true;
+let lastMove = '';
+export const RIGHT = "right";
+export const RIGHT_UP = "rightUp";
+export const RIGHT_DOWN = "rightDown";
 
 /**
  * Replace the logic in this function with your own custom movement algorithm.
@@ -17,25 +21,184 @@ let movedRight;
  * @return {Position} The new position of the miner.
  */
 const move = (mine, position) => {
-  // TODO: write logic for miner. The current approach naive approach is to simply:
-  //   1. Start at (0,0)
-  //   2. Always moves right
+  const mineLength = {
+    y: mine.length,
+    x: mine.reduce((x, y) => Math.max(x, y.length), 0)
+  };
 
-  const newX = (position && position.x + 1) || 0;
+  console.log('start ------------------------------------');
+  console.log('length: ', mineLength);
+  console.log('start: ', position);
+  console.log('firstMove: ', firstMove);
+  console.log('lastmove: ', firstMove);
 
-  let newY;
+  if (firstMove) {
+    firstMove = false;
 
-  if (!movedRight) {
-    newY = (position && position.y) || 0;
+    const initialPosition = getFirstPosition(mine);
 
-    movedRight = true;
-  } else {
-    newY = (position && position.y + 1) || 0;
+    console.log('end: ', initialPosition);
+    console.log('firstMove end: ', firstMove);
+    console.log('lastmove end: ', lastMove);
+    console.log('value: ', mine[initialPosition.y][initialPosition.x]);
+    console.log('end ------------------------------------');
 
-    movedRight = false;
+    return initialPosition;
   }
 
-  return new Position(newX, newY);
+  const { nextPosition, move } = getNextPosition(mine, lastMove, position);
+  lastMove = move;
+
+  if (nextPosition.x >= mineLength.x-1) {
+    firstMove = true;
+  }
+
+  if (nextPosition.y > mineLength.y) {
+    firstMove = true;
+  }
+
+  if (mine[nextPosition.y][nextPosition.x] === 0) {
+    firstMove = true;
+  }
+
+  console.log('end: ', nextPosition);
+  console.log('firstMove end: ', firstMove);
+  console.log('lastmove end: ', lastMove);
+  console.log('value: ', mine[nextPosition.y][nextPosition.x]);
+  console.log('end ------------------------------------');
+
+  return nextPosition;
+};
+
+export const positionIsSafe = (mine, position, nextMove) => {
+  const mineLength = {
+    y: mine.length,
+    x: mine.reduce((x, y) => Math.max(x, y.length), 0)
+  };
+  const rightValue = (position.x+1 < mineLength.x) ? mine[position.y][position.x+1] : 0;
+  const rightUpValue = (position.y-1 > -1 && position.x+1 < mineLength.x) ? mine[position.y-1][position.x+1] : 0;
+  const rightDownValue = (position.y+1 < mineLength.x && position.x+1 < mineLength.x) ? mine[position.y+1][position.x+1] : 0;
+
+  switch (nextMove) {
+    case RIGHT_UP:
+      if (rightValue !== 0 || rightDownValue !== 0) {
+        return true;
+      }
+      break;
+
+    case RIGHT_DOWN:
+      if (rightValue !== 0 ||rightUpValue !== 0) {
+        return true;
+      }
+      break;
+
+    case RIGHT:
+      if (rightUpValue !== 0 || rightDownValue !== 0) {
+        return true;
+      }
+      break;
+
+    default:
+      if (rightValue !== 0 || rightUpValue !== 0 || rightDownValue !== 0) {
+        return true;
+      }
+      break;
+  }
+
+  return false;
+};
+
+export const getFirstPosition = (mine) => {
+  const highestNumbersRows = mine.map(function(row){ return Math.max.apply(Math, row); });
+  const highestNumber = highestNumbersRows.reduce((firstElem, secElem) => Math.max(firstElem, secElem));
+  const rowsHighestNumberCount = [];
+  let elementCount;
+
+  for (let i = 0; i < mine.length; i++) {
+    elementCount = 0;
+
+    for (let j = 0; j < mine[i].length; j++) {
+      if (mine[i][j] === highestNumber) {
+        elementCount++;
+      }
+    }
+
+    rowsHighestNumberCount.push([i, elementCount]);
+  }
+
+  rowsHighestNumberCount.sort((firstElem, secElem) => firstElem[1] > secElem[1] ? -1 : 1);
+
+  for (let i = 0; i < rowsHighestNumberCount.length; i++) {
+    const initialPosition = new Position(0, rowsHighestNumberCount[i][0]);
+
+    if (positionIsSafe(mine, initialPosition, '')) {
+      return initialPosition;
+    }
+  }
+};
+
+export const getNextPosition = (mine, lastMove, position) => {
+  const rightPosition = new Position(position.x+1, position.y);
+  const rightUpPosition = new Position(position.x+1, position.y-1);
+  const rightDownPosition = new Position(position.x+1, position.y+1);
+  let nextPositions = [];
+
+  switch (lastMove) {
+    case RIGHT_UP:
+      if (rightPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightPosition, value: mine[rightPosition.y][rightPosition.x], move: RIGHT });
+      }
+
+      if (rightDownPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightDownPosition, value: mine[rightDownPosition.y][rightDownPosition.x], move: RIGHT_DOWN });
+      }
+      break;
+
+    case RIGHT_DOWN:
+      if (rightPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightPosition, value: mine[rightPosition.y][rightPosition.x], move: RIGHT });
+      }
+
+      if (rightUpPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightUpPosition, value: mine[rightUpPosition.y][rightUpPosition.x], move: RIGHT_UP });
+      }
+      break;
+
+    case RIGHT:
+      if (rightDownPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightDownPosition, value: mine[rightDownPosition.y][rightDownPosition.x], move: RIGHT_DOWN });
+      }
+
+      if (rightUpPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightUpPosition, value: mine[rightUpPosition.y][rightUpPosition.x], move: RIGHT_UP });
+      }
+      break;
+
+    default:
+      if (rightPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightPosition, value: mine[rightPosition.y][rightPosition.x], move: RIGHT });
+      }
+
+      if (rightUpPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightUpPosition, value: mine[rightUpPosition.y][rightUpPosition.x], move: RIGHT_UP });
+      }
+
+      if (rightDownPosition.isValid(mine)) {
+        nextPositions.push({ nextPosition: rightDownPosition, value: mine[rightDownPosition.y][rightDownPosition.x], move: RIGHT_DOWN });
+      }
+      break;
+  }
+
+  if (nextPositions.length > 0) {
+    nextPositions.sort((firstElem, secElem) => firstElem.value > secElem.value ? -1 : 1);
+
+    for (let i = 0; i < nextPositions.length; i++) {
+      if (positionIsSafe(mine, nextPositions[i].nextPosition, nextPositions[i].move)) {
+        return nextPositions[i];
+      }
+    } 
+  }
+  return { nextPosition: (lastMove === RIGHT) ? rightDownPosition : rightPosition, move: (lastMove === RIGHT) ? RIGHT_DOWN : RIGHT };
 };
 
 export default move;
--- src/move.test.js
+++ src/move.test.js
@@ -1,5 +1,178 @@
-describe('My Test Suite', () => {
-  it('My Test Case', () => {
-    expect(true).toEqual(true);
+import move, { getFirstPosition, getNextPosition, positionIsSafe, RIGHT, RIGHT_UP, RIGHT_DOWN } from "./move.js";
+import Position from "./position.js";
+
+describe('move suite case', () => {
+  describe('positionIsSafe suite case', () => {
+    describe('when calls positionIsSafe with lastMove right up', () => {
+      it('returns true if the next move down or next move right is not zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 9, 3],
+          [9, 0, 4, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, 'rightUp')).toEqual(true);
+      });
+      it('returns false if the next move down or next move right is zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 0, 3],
+          [9, 0, 0, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, 'rightUp')).toEqual(false);
+      });
+    });
+
+    describe('when calls positionIsSafe with lastMove right down', () => {
+      it('returns true if the next move up or next move right is not zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 9, 3],
+          [9, 0, 4, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, 'rightUp')).toEqual(true);
+      });
+      it('returns false if the next move up or next move right is zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 0, 3],
+          [9, 0, 0, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, 'rightDown')).toEqual(false);
+      });
+    });
+
+    describe('when calls positionIsSafe with lastMove right', () => {
+      it('returns true if the next move up or next move down is not zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 9, 3],
+          [9, 0, 4, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, 'right')).toEqual(true);
+      });
+      it('returns false if the next move up or next move down is zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 0, 3],
+          [9, 0, 0, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, 'right')).toEqual(false);
+      });
+    });
+
+    describe('when calls positionIsSafe with lastMove empty', () => {
+      it('returns true if the next move up or next move down or next move right is not zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 9, 3],
+          [9, 0, 0, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, '')).toEqual(true);
+      });
+      it('returns false if the next move up or next move down or next move right is zero', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 0, 3],
+          [9, 0, 0, 1],
+          [1, 0, 8, 5],
+        ];
+
+        const position = new Position(1, 1);
+
+        expect(positionIsSafe(multArr, position, '')).toEqual(false);
+      });
+    });
+  });
+
+  describe('getFirstPosition suite case', () => {
+    describe('when calls getFirstPosition function of a multidimensional array', () => {
+      it('returns position row that has more duplicate of the highest multidimensional array value', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 9, 3],
+          [9, 0, 9, 9],
+          [1, 0, 8, 5],
+        ];
+
+        const initialPosition = getFirstPosition(multArr);
+
+        expect(initialPosition.y).toEqual(2);
+      });
+
+      it('returns the next position row that has more duplicate of the highest multidimensional array value while position safe is not found', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 0, 9, 3],
+          [9, 0, 9, 9],
+          [1, 0, 8, 5],
+        ];
+
+        const initialPosition = getFirstPosition(multArr);
+
+        expect(initialPosition.y).toEqual(1);
+      });
+    });
+  });
+
+  describe('getNextPosition suite case', () => {
+    describe('when calls getNextPosition function with the current position', () => {
+      it('returns next position and the move that is safe when is first move', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 9, 3],
+          [9, 0, 9, 9],
+          [1, 0, 8, 5],
+        ];
+
+        const { nextPosition, move } = getNextPosition(multArr, '', new Position(0,2));
+
+        expect(nextPosition.y).toEqual(1);
+        expect(nextPosition.x).toEqual(1);
+        expect(move).toEqual(RIGHT_UP);
+      });
+
+      it('returns next position and the move that is safe when the last move is up', () => {
+        const multArr = [
+          [6, 3, 0, 2],
+          [8, 8, 7, 3],
+          [9, 0, 9, 9],
+          [1, 0, 8, 5],
+        ];
+
+        const { nextPosition, move } = getNextPosition(multArr, RIGHT_UP, new Position(1,1));
+
+        expect(nextPosition.y).toEqual(2);
+        expect(nextPosition.x).toEqual(2);
+        expect(move).toEqual(RIGHT_DOWN);
+      });
+    });
   });
 });
