54ce39f6e6c0fea462b689ee33b20cbde58a46ef
Alexander Mark <alex.mike.mark.again@gmail.com>
2022-02-23 13:13:38 -0600

    One more comment

--- src/oracleMoves.js
+++ src/oracleMoves.js
@@ -2,3 +2,5 @@ export const UP = Symbol("up");
 export const MID = Symbol("MID");
 export const DOWN = Symbol("DOWN");
 export const NONE = Symbol("NONE");
+
+// this is in its own file so we can mess with these values for the tests. It's not ideal.
